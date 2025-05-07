d81967d2aec5af6376e5d14e1e91762b7b2e0daa
Johnny Nilsson <johnny.nilsson83@gmail.com>
2025-05-07 17:13:00 +0200

    Add test to validate written logs with markers can be found with contains.

--- src/test/java/nl/altindag/log/model/LogMarkerShould.java
+++ src/test/java/nl/altindag/log/model/LogMarkerShould.java
@@ -1,8 +1,14 @@
 package nl.altindag.log.model;
 
+import nl.altindag.log.LogCaptor;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,5 +49,32 @@ public class LogMarkerShould {
 
         assertThat(markerA).isNotEqualTo(markerB);
         assertThat(markerA.hashCode()).isNotEqualTo(markerB.hashCode());
+    }
+
+    @Test
+    void whenInACollectionReturnTrueWhenFoundInCollection() {
+        LogMarker markerA = new LogMarker("MARKER", null);
+        LogMarker markerB = new LogMarker("MARKER", null);
+        List<LogMarker> markers = new ArrayList<>();
+        markers.add(markerA);
+
+        assertThat(markers).containsExactly(markerB);
+    }
+
+    @Test
+    void beFoundViaContainsWhenLogged() {
+        LogCaptor logCaptor = LogCaptor.forClass(LogMarkerShould.class);
+        Logger logger = LoggerFactory.getLogger(LogMarkerShould.class);
+
+        Marker marker = MarkerFactory.getDetachedMarker("MARKER");
+        marker.add(MarkerFactory.getDetachedMarker("REF_MARKER"));
+        logger.info(marker, "This is a test");
+
+        List<LogMarker> refs = new ArrayList<>();
+        refs.add(new LogMarker("REF_MARKER", new ArrayList<>()));
+        LogMarker expected = new LogMarker("MARKER", refs);
+
+        boolean anyMatch = logCaptor.getLogEvents().stream().anyMatch(event -> event.getMarkers().contains(expected));
+        assertThat(anyMatch).isTrue();
     }
 }
